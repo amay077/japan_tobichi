36af4d5eb8b1a8d38c24624651552d61677bdd7f
amay077 <okuokuoku@gmail.com>
2020-01-28 01:59:51 +0900

    up

--- tool/README.md
+++ tool/README.md
@@ -9,15 +9,14 @@
 
 # Getting Started
 
-1. [地球地図日本データ](https://www.gsi.go.jp/kankyochiri/gm_jpn.html)
-2. 第2.2版ベクタ（2016年公開）の「全レイヤ」をダウンロードする
-3. ファイルを解凍して ``polbnda_jpn.*`` だけを圧縮して ``polbnda_jpn.zip`` とする
-5. このリポジトリを Clone する
-6. ``polbnda_jpn.zip`` を ``tool/assets`` ディレクトリに置く
-6. ``tool`` ディレクトリで ``npm ci`` を行う
-8. ``npm run exec`` を行う
-9. ・・・しばらく待つ
-10. ``../out`` ディレクトリに ``*.geojson`` ファイルが出力される
+1. [地球地図日本データ](https://www.gsi.go.jp/kankyochiri/gm_jpn.html) 第2.2版ベクタ（2016年公開）の「全レイヤ」をダウンロードする
+2. ファイルを解凍して ``polbnda_jpn.*`` だけを圧縮して ``polbnda_jpn.zip`` とする
+3. このリポジトリを Clone する
+4. ``polbnda_jpn.zip`` を ``tool/assets`` ディレクトリに置く
+5. ``tool`` ディレクトリで ``npm ci`` を行う
+6. ``npm run exec`` を行う
+7. ・・・しばらく待つ
+8. ``../out`` ディレクトリに ``*.geojson`` ファイルが出力される
 
 * ``tobichi_00_all.geojson`` … すべての飛び地とその本体ポリゴンが格納された GeoJson ファイル
 * ``tobichi_{nn}.geojson`` … 都道府県ごとの飛び地とその本体ポリゴンが格納された GeoJson ファイル
--- tool/src/tobichi-extractor.ts
+++ tool/src/tobichi-extractor.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs-extra'
 import * as Path from 'path'
 import * as shp from 'shpjs';
 import * as turf from '@turf/turf';
-import { Position, Feature, Geometry, feature, Polygon } from '@turf/turf';
+import { Feature, Polygon } from '@turf/turf';
 import * as randomColor from 'randomcolor';
 import * as Enumerable from 'linq';
 
@@ -18,7 +18,7 @@ export class TobichiExtractor {
       const geoJson = await shp(buf);
 
       const sourceFeatures: Feature[] = geoJson.features
-        // .filter(f => f.properties.adm_code.startsWith('01')) // 北海道のみ
+        // .filter(f => f.properties.adm_code.startsWith('4')) // 北海道のみ
         ;
 
       // 市区町村の本体ポリゴンのみのリストと
@@ -47,47 +47,6 @@ export class TobichiExtractor {
       const colors = randomColor({
         luminosity: 'dark',
         count: 100
-      });
-
-      // 本体ポリゴンを足して MultiPolygon 化
-      // 例: {
-      //   "geometry": {
-      //     "type": "MultiPolygon",
-      //     "coordinates": [
-      //       [[[1,1], [1,2]...]], // 本体ポリゴン
-      //       [[[2,1], [2,2]...]], // 飛び地ポリゴン1
-      //       [[[3,1], [3,2]...]], // 飛び地ポリゴン2...
-      //     ]
-      //   },
-      // }
-      const tobichiMultiPolygonFeatures = Array.from(tobichiSubFeatures.reduce((pre, cur) => {
-        const arr = pre.get(cur.properties.adm_code);
-        if (arr != null) {
-          pre.set(cur.properties.adm_code, [...arr, cur]);
-        } else {
-          const mainF = mainFeatures.find(f => f.properties.adm_code === cur.properties.adm_code);
-          pre.set(cur.properties.adm_code, [mainF, cur]);
-        }
-
-        return pre;
-      }, new Map<string, Feature[]>()).values())
-      .map((features, index) => {
-        const mainF = features.find(f => f.properties.pop >= 0);
-
-        const multiCoordinates = features.map(f => (f.geometry as Geometry).coordinates) as Position[][][];
-        const multiPolygonF = turf.multiPolygon(multiCoordinates, mainF.properties);
-       
-        const color = colors[index % colors.length];
-        multiPolygonF.properties.fill = color;
-        multiPolygonF.properties.stroke = color;
-
-        multiPolygonF.properties._opacity =  1;
-        multiPolygonF.properties._weight = 3;
-        multiPolygonF.properties._fillOpacity = 0.2;
-        multiPolygonF.properties._fillColor = color;
-        multiPolygonF.properties._color = color;
-
-        return multiPolygonF;
       });
 
       // １つの市区町村につき複数の飛び地があるかも知れないので、
@@ -134,69 +93,7 @@ export class TobichiExtractor {
       }
       console.log(`Found ${tobichiSubFeatures.length} Tobichis -----`);
 
-      console.log(`Statistics ---`);
-
-      // 面積最大最小
-      const featureWithAreas = Enumerable.from(tobichiSubFeatures).select(f => {
-        f.properties.area = turf.area(f);
-        return f;
-      })
-      .orderBy(f => f.properties.area);
-      const areaMins = featureWithAreas.take(3);
-      const areaMaxs = featureWithAreas.reverse().take(3);
-      console.log(`Area-min --`);
-      areaMins.toArray().forEach((f, index) => {
-        console.log(`${index+1}. ${f.properties.adm_code}/${f.properties.nam}/${f.properties.laa} - ${f.properties.area} ㎡`);
-      });
-      console.log(`Area-max --`);
-      areaMaxs.toArray().forEach((f, index) => {
-        console.log(`${index+1}. ${f.properties.adm_code}/${f.properties.nam}/${f.properties.laa} - ${f.properties.area} ㎡`);
-      });
-
-      // 飛び地数最多
-      const featureWithNumTobichis = Enumerable.from(Array.from(admCodeFeaturesMap.entries())).select(([admCode, features]) => ({
-        properties: features[0].properties,
-        numFeatures: features.length
-      }))
-      .orderByDescending(x => x.numFeatures)
-      .take(3);
-      console.log(`Num tobichi-max --`);
-      featureWithNumTobichis.toArray().forEach((f, index) => {
-        console.log(`${index+1}. ${f.properties.adm_code}/${f.properties.nam}/${f.properties.laa} - ${f.numFeatures} tobichis`);
-      });
-
-      // 本体との距離
-      const featureWithDists = Enumerable.from(Array.from(admCodeFeaturesMap.values())).select(features => {
-        const mainF = features.find(f => f.properties.pop >= 0);
-        const mainCoords = Enumerable.from((mainF.geometry as Polygon).coordinates[0]);
-
-        const distMainToSubMin = Enumerable.from(features)
-          .where(f => f.properties.pop < 0)
-          .select(f => mainCoords.min(coord => {
-            const outer = turf.lineString((f.geometry as Polygon).coordinates[0]);
-            const closest = turf.nearestPointOnLine(outer, coord);
-            return closest.properties.dist;
-          }))
-          .min();
-
-        return {
-          adm_code: mainF.properties.adm_code,
-          nam: mainF.properties.nam,
-          laa: mainF.properties.laa,
-          dist: distMainToSubMin
-        };
-      })
-      .orderBy(f => f.dist);
-
-      console.log(`Dist-min --`);
-      featureWithDists.take(3).toArray().forEach((f, index) => {
-        console.log(`${index+1}. ${f.adm_code}/${f.nam}/${f.laa} - ${f.dist} km`);
-      });
-      console.log(`Dist-max --`);
-      featureWithDists.reverse().take(3).toArray().forEach((f, index) => {
-        console.log(`${index+1}. ${f.adm_code}/${f.nam}/${f.laa} - ${f.dist} km`);
-      });
-
+      this.dumpStatistics(admCodeFeaturesMap, tobichiSubFeatures);
 
       // 全国の .geojson ファイル出力
       const resultGeoJson = {
@@ -227,5 +124,113 @@ export class TobichiExtractor {
     }
 
     return 0;
+  }
+
+  private dumpStatistics(admCodeFeaturesMap: Map<string, Feature[]>, tobichiSubFeatures: Feature[]) {
+    console.log(`Statistics ---`);
+
+    const tobichiNumPerPref = Enumerable.from(Array.from(admCodeFeaturesMap.entries())).select(([admCode, features]) => {
+      return {
+        admCode: admCode,
+        featureNum: features.length
+      }
+    })
+    .groupBy(x => x.admCode.substring(0, 2))
+    .select(g =>  ({ pref: g.key(), featureNum: g.sum(x => x.featureNum)}))
+    .orderByDescending(x => x.featureNum)
+    .toArray();
+    console.log(`Tobichi num per pref --`);
+    tobichiNumPerPref.forEach((x, index) => {
+      console.log(`${index+1}. ${x.pref} - ${x.featureNum} tobichis`);
+    });
+
+    // 面積最大最小
+    const featureWithAreas = Enumerable.from(tobichiSubFeatures).select(f => {
+      f.properties.area = turf.area(f);
+      return f;
+    })
+    .orderBy(f => f.properties.area);
+    const areaMins = featureWithAreas.take(3);
+    const areaMaxs = featureWithAreas.reverse().take(3);
+    console.log(`Area-min --`);
+    areaMins.toArray().forEach((f, index) => {
+      console.log(`${index+1}. ${f.properties.adm_code}/${f.properties.nam}/${f.properties.laa} - ${f.properties.area} ㎡`);
+    });
+    console.log(`Area-max --`);
+    areaMaxs.toArray().forEach((f, index) => {
+      console.log(`${index+1}. ${f.properties.adm_code}/${f.properties.nam}/${f.properties.laa} - ${f.properties.area} ㎡`);
+    });
+
+    // 飛び地数最多
+    const featureWithNumTobichis = Enumerable.from(Array.from(admCodeFeaturesMap.entries())).select(([admCode, features]) => ({
+      properties: features[0].properties,
+      numFeatures: features.length
+    }))
+    .orderByDescending(x => x.numFeatures)
+    .take(3);
+    console.log(`Num tobichi-max --`);
+    featureWithNumTobichis.toArray().forEach((f, index) => {
+      console.log(`${index+1}. ${f.properties.adm_code}/${f.properties.nam}/${f.properties.laa} - ${f.numFeatures} tobichis`);
+    });
+
+    // 本体との距離
+    const featureWithDists = Enumerable.from(Array.from(admCodeFeaturesMap.values())).select(features => {
+      const mainF = features.find(f => f.properties.pop >= 0);
+      const mainCoords = Enumerable.from((mainF.geometry as Polygon).coordinates[0]);
+
+      const distMainToSubMin = Enumerable.from(features)
+        .where(f => f.properties.pop < 0)
+        .select(f => mainCoords.min(coord => {
+          const outer = turf.lineString((f.geometry as Polygon).coordinates[0]);
+          const closest = turf.nearestPointOnLine(outer, coord);
+          return closest.properties.dist;
+        }))
+        .min();
+
+      return {
+        adm_code: mainF.properties.adm_code,
+        nam: mainF.properties.nam,
+        laa: mainF.properties.laa,
+        dist: distMainToSubMin
+      };
+    })
+    .orderBy(f => f.dist);
+
+    console.log(`Dist-min --`);
+    featureWithDists.take(3).toArray().forEach((f, index) => {
+      console.log(`${index+1}. ${f.adm_code}/${f.nam}/${f.laa} - ${f.dist} km`);
+    });
+    console.log(`Dist-max --`);
+    featureWithDists.reverse().take(3).toArray().forEach((f, index) => {
+      console.log(`${index+1}. ${f.adm_code}/${f.nam}/${f.laa} - ${f.dist} km`);
+    });
+
+    // 本体より面積が大きい飛び地
+    const featureAreas = Enumerable.from(Array.from(admCodeFeaturesMap.values())).select(features => {
+      const mainF = features.find(f => f.properties.pop >= 0);
+      const mainArea = turf.area(mainF);
+
+      const subAreas = Enumerable.from(features)
+        .where(f => f.properties.pop < 0)
+        .select(f => turf.area(f))
+        .toArray();
+
+      return {
+        adm_code: mainF.properties.adm_code,
+        nam: mainF.properties.nam,
+        laa: mainF.properties.laa,
+        mainArea: mainArea,
+        subAreas: subAreas
+      }
+    })
+    .where(x => {
+      return x.subAreas.find(subArea => x.mainArea < subArea) != null;
+    })
+    .toArray();
+
+    console.log(`Sub area greater than main area --`);
+    featureAreas.forEach((f, index) => {
+      console.log(`${index+1}. ${f.adm_code}/${f.nam}/${f.laa} - ${f.mainArea} ㎡ < ${f.subAreas.join(',')} ㎡`);
+    });      
   }
 }  
